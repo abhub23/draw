8cb1b031afe4b0bb5f51707d7c02857c56f9d36c
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-10-25 23:53:23 +0530

    One state practice / experiment

--- store/useDrawing.ts
+++ store/useDrawing.ts
@@ -2,10 +2,20 @@ import { create } from 'zustand';
 
 type Drawtype = {
   isDrawing: boolean;
+  color: string;
+  strokeWidth: number;
+
   setIsDrawing: (value: boolean) => void;
+  setColor: (value: string) => void;
+  setStrokeWidth: (value: number) => void;
 };
 
 export const useDrawing = create<Drawtype>((set) => ({
   isDrawing: false,
+  strokeWidth: 3,
+  color: '#000000',
+  
   setIsDrawing: (value: boolean) => set({ isDrawing: value }),
+  setStrokeWidth: (value: number) => set({ strokeWidth: value }),
+  setColor: (value: string) => set({ color: value }),
 }));
