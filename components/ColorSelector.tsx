1e044e9bff5ec78618266ee1ee10630f9315cea9
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2025-11-01 23:54:56 +0530

    chore

--- components/ColorSelector.tsx
+++ components/ColorSelector.tsx
@@ -4,15 +4,13 @@ import { useDrawing } from '@/store/useDrawing';
 export default function ColorSelector() {
   const { color, setColor } = useDrawing();
   return (
-    <div className='absolute z-10'>
-      <Github
-        className='gap-1 lg:min-w-[240px]'
-        color={color}
-        showTriangle={true}
-        onChange={(color) => {
-          setColor(color.hex);
-        }}
-      />
-    </div>
+    <Github
+      className='gap-1 lg:min-w-[240px]'
+      color={color}
+      showTriangle={true}
+      onChange={(color) => {
+        setColor(color.hex);
+      }}
+    />
   );
 }
