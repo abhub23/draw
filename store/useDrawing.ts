2e9cece756ab6a1288294fa7b3aa79b8bbb36b73
abdullahmukri25@gmail.com <91679434+Abdullah-Github25@users.noreply.github.com>
2026-02-22 00:26:26 +0530

    added clear state

--- store/useDrawing.ts
+++ store/useDrawing.ts
@@ -5,19 +5,23 @@ type DrawType = {
   isDrawing: boolean;
   color: string;
   strokeWidth: number;
+  clearTimestamp: number;
 
   // state updating methods
   setIsDrawing: (value: boolean) => void;
   setColor: (value: string) => void;
   setStrokeWidth: (value: number) => void;
+  clearCanvas: () => void;
 };
 
 export const useDrawing = create<DrawType>((set) => ({
   isDrawing: false,
   strokeWidth: 3,
   color: '#000000',
+  clearTimestamp: 0,
 
   setIsDrawing: (value: boolean) => set({ isDrawing: value }),
   setStrokeWidth: (value: number) => set({ strokeWidth: value }),
   setColor: (value: string) => set({ color: value }),
+  clearCanvas: () => set((state) => ({ isDrawing: false, clearTimestamp: state.clearTimestamp + 1 })),
 }));
